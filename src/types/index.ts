ec0e2cb0efcb70dc3a2f7ea45a43f2050244dfca
Konstantin Tsepelev <constantine@tsepelev.com>
2020-03-27 21:17:20 +0300

    feat: add typings for queues

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export * from './responses.types';
 export * from './task.types';
 export * from './worker.types';
 export * from './worker-pool.types';
+export * from './queue.type';
---
+++ src/types/queue.type.ts
@@ -0,0 +1,9 @@
+type SyncOrAsync<T> = Promise<T> | T;
+
+export interface WorkerPoolQueueInterface<T> {
+  add(item: T, ...args: unknown[]): SyncOrAsync<this>;
+  clear(): SyncOrAsync<void>;
+  isEmpty(): SyncOrAsync<boolean>;
+  poll(): SyncOrAsync<T>;
+  toString?(callback: Function): SyncOrAsync<string>;
+}
